5cf32ee26385d340714448b0b14f198d8c8b297a
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-26 17:36:32 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/zupacademy/mateus/Propostas/PropostasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.mateus.Propostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostasApplication.class, args);
+	}
+
+}
