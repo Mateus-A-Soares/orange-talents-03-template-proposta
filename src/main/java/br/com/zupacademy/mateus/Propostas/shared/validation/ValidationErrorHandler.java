b833038be93c439d577e60e50679e44b7d1f0eae
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-28 11:51:47 -0300

    Atualização das respostas de erro enviadas pela aplicação

--- src/main/java/br/com/zupacademy/mateus/Propostas/shared/validation/ValidationErrorHandler.java
+++ src/main/java/br/com/zupacademy/mateus/Propostas/shared/validation/ValidationErrorHandler.java
@@ -7,15 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import br.com.zupacademy.mateus.Propostas.shared.validation.response.ErrorMessageResponse;
 import br.com.zupacademy.mateus.Propostas.shared.validation.response.ErrorMessageItem;
@@ -66,6 +70,55 @@ public class ValidationErrorHandler {
 		BindingResult bindingResult = exception.getBindingResult();
 		return handle(bindingResult, request);
 	}
+	
+	/**
+	 *  Invocado quando a aplicação não encontra um end point relativo a URL da request,
+	 * popula um {@link ErrorMessageResponse} para ser serializado como resposta para o usuário final.
+	 * 
+	 * @param request	representação da request da qual vai ser tirada o caminho que o usuário acessou;
+	 * @return {@link ErrorMessageResponse} populado com o erro 404 e URI da request para ser serializado como resposta.
+	 */
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ErrorMessageResponse handleNotFound(WebRequest request) {
+		String errorMessage = messageSource.getMessage("NotFound", null, "404 Not Found", null);
+		String path = ((ServletWebRequest) request).getRequest().getRequestURI().toString();
+		ErrorMessageItem error = new ErrorMessageItem("path", errorMessage);
+		return new ErrorMessageResponse(HttpStatus.NOT_FOUND, path, error);
+	}
+	
+	/**
+	 *  Invocado quando o end point relativo a URL da request não suporta o método definido na request,
+	 * popula um {@link ErrorMessageResponse} para ser serializado como resposta para o usuário final.
+	 * 
+	 * @param request	representação da request da qual vai ser tirada o caminho que o usuário acessou;
+	 * @return {@link ErrorMessageResponse} populado com o erro 405 e URI da request para ser serializado como resposta.
+	 */
+	@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ErrorMessageResponse handleMethodNotAllowed(WebRequest request) {
+		String errorMessage = messageSource.getMessage("MethodNotAllowed", null, "405 Method Not Allowed", null);
+		String path = ((ServletWebRequest) request).getRequest().getRequestURI().toString();
+		ErrorMessageItem error = new ErrorMessageItem("method", errorMessage);
+		return new ErrorMessageResponse(HttpStatus.METHOD_NOT_ALLOWED, path, error);
+	}
+	
+	
+	/**
+	 *  Invocado quando a implementação ativa do {@link HttpMessageConverter} não consegue ler o corpo da requisição enviada para algum end point da aplicação,
+	 * popula um {@link ErrorMessageResponse} para ser serializado como resposta para o usuário final.
+	 * 
+	 * @param request	representação da request da qual vai ser tirada o caminho que o usuário acessou;
+	 * @return {@link ErrorMessageResponse} populado com o erro 422 e URI da request para ser serializado como resposta.
+	 */
+	@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ErrorMessageResponse handleUnprocessableEntity(WebRequest request) {
+		String errorMessage = messageSource.getMessage("UnprocessableEntity", null, "422 Unprocessable Entity", null);
+		String path = ((ServletWebRequest) request).getRequest().getRequestURI().toString();
+		ErrorMessageItem error = new ErrorMessageItem("request body", errorMessage);
+		return new ErrorMessageResponse(HttpStatus.UNPROCESSABLE_ENTITY, path, error);
+	}
 
 	/**
 	 *  Invocado quando qualquer exception não tratada / esperada for lançada,
@@ -76,8 +129,8 @@ public class ValidationErrorHandler {
 	 */
 	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
-	public ErrorMessageResponse handle(WebRequest request) {
-		String errorMessage = messageSource.getMessage("InternalServerError", null, "500", null);
+	public ErrorMessageResponse handleInternalServerError(WebRequest request) {
+		String errorMessage = messageSource.getMessage("InternalServerError", null, "500 Internal Server Error", null);
 		String path = ((ServletWebRequest) request).getRequest().getRequestURI().toString();
 		ErrorMessageItem error = new ErrorMessageItem("server", errorMessage);
 		return new ErrorMessageResponse(HttpStatus.INTERNAL_SERVER_ERROR, path, error);
