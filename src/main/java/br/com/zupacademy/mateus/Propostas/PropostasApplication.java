c86b470e138f25c7d24b2d48d424ed73f0afc8bc
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-29 14:05:01 -0300

    Desabilitando o contexto transacional padrão das chamadas de método dos repositórios

--- src/main/java/br/com/zupacademy/mateus/Propostas/PropostasApplication.java
+++ src/main/java/br/com/zupacademy/mateus/Propostas/PropostasApplication.java
@@ -3,9 +3,11 @@ package br.com.zupacademy.mateus.Propostas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class PropostasApplication {
 
 	public static void main(String[] args) {
